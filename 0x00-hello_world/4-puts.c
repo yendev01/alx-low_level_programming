035eb815671ca011aa3737be0327acf486300e91
Sydney <yendev01@gmail.com>
2022-10-16 11:49:32 +0000

    printing out on c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this program prints out strings using puts'
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void) /*function which prints out strings using puts*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
