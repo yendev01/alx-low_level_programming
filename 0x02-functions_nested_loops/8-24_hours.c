c6c9d05443d1ee79d378c84a300ccb847ec8d536
Sydney <yendev01@gmail.com>
2022-10-19 15:18:34 +0000

    print out every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * jack_bauer - entry point
+  * description: printing digit combinations
+  * Return: returns void
+  */
+
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while (i < 3)
+	{
+		int n = 0;
+
+		while (n < 4)
+		{
+			int v = 0;
+
+			while (v < 6)
+			{
+				int m = 0;
+
+				while (m < 10)
+				{
+					_putchar(48 + i);
+					_putchar(48 + n);
+					_putchar(':');
+					_putchar(48 + v);
+					_putchar(48 + m);
+					_putchar('\n');
+					m++;
+				}
+
+				v++;
+			}
+			n++;
+		}
+		i++;
+	}
+}
