ae05018c6c9658c302fdd70bbaeaa62d8646f60f
Sydney <yendev01@gmail.com>
2022-10-19 17:05:10 +0000

    9x9 table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+  * times_table - entry point
+  * description: 9x9 table
+  * Return: returns void
+  */
+
+void times_table(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int j = 0;
+
+		while (j < 10)
+		{
+			int p = i * j;
+
+			if (j == 0)
+			{
+				_putchar(p + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+			
+			else if (j == 9)
+			{
+				if (p < 9)
+				{
+					_putchar(' ');
+					_putchar(p + 48);
+				}
+				else
+				{
+					_putchar(p / 10 + 48);
+					_putchar(p % 10 + 48);
+				}
+			}
+
+			else
+			{
+				if (p >= 10)
+				{
+					_putchar(p / 10 + 48);
+					_putchar(p % 10 + 48);
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(p + 48);
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+
+			j++;
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
