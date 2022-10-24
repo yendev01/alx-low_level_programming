614bcbf4218628591ba7aa23111be55641ea3a51
Sydney <yendev01@gmail.com>
2022-10-24 19:09:47 +0000

    prints strings in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+  * print_rev - entry point
+  * description: function that prints a string in a reverse
+  * @s: input to function
+  * Return: nothing
+  */
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+
+	while (len >= 0)
+	{
+		_putchar(s[len]);
+
+		len--;
+	}
+
+	_putchar('\n');
+}
+
+/**
+  * _strlen - entry point
+  * description: function that calculates the length of a string
+  * Return: returns length of a string
+  * @s: input(string)
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
