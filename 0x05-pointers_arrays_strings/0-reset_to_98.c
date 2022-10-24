66cd1caac2f52f3ab194028fe4ef1d2f6f1fb2af
Sydney <yendev01@gmail.com>
2022-10-24 16:12:23 +0000

    pointer and data structure

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - entry point
+  * description: takes and updates the pointer value to 98
+  * Return: nothing
+  * @n: pointer input
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
