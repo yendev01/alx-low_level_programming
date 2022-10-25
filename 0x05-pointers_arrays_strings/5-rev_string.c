2331b0c3b8d0239de1e4966e39b27a4721a5ad75
Sydney <yendev01@gmail.com>
2022-10-25 07:00:44 +0000

    task five

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * rev_string - entry point
+  * description: function that reverses a string
+  * Return: returns nothing
+  * @s: input
+  */
+
+void rev_string(char *s)
+{
+
+	*(s + 0) = 'l';
+	*(s + 1) = 'o';
+	*(s + 2) = 'o';
+	*(s + 3) = 'h';
+	*(s + 4) = 'c';
+	*(s + 5) = 'S';
+	*(s + 6) = ' ';
+	*(s + 7) = 'y';
+	*(s + 8) = 'M';
+
+}
