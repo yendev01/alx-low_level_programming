21c4c76aad72198ffd2f27e52f078877bdf6733c
Sydney <yendev01@gmail.com>
2022-10-26 16:44:37 +0000

    dynamic memory allocation

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,55 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <stdlib.h>
 /**
   * rev_string - entry point
   * description: function that reverses a string
   * Return: returns nothing
-  * @s: input
+  * @s: input to function
   */
 
-void rev_string(char *s)
+
+int _strlen(char *s);
+
+void rev_string(char *str)
 {
+	int i, n = 0;
+/*	char v[500];*/
+	char *v;
 
-	*(s + 0) = 'l';
-	*(s + 1) = 'o';
-	*(s + 2) = 'o';
-	*(s + 3) = 'h';
-	*(s + 4) = 'c';
-	*(s + 5) = 'S';
-	*(s + 6) = ' ';
-	*(s + 7) = 'y';
-	*(s + 8) = 'M';
+	v = calloc(_strlen(str), sizeof(char));
 
+	while (str[n] != '\0')
+	{
+		*(v + n) = str[n];
+		n++;
+	}
+	*(v + n) = '\0';
+
+	i = n - 1;
+
+	while (i >= 0)
+	{
+		*str = *(v + i);
+		str++;
+		i--;
+	}
+}
+
+/**
+  * _strlen - entry point
+  * description: function that calculates the length of a string
+  * Return: returns length of a string
+  * @s: input(string)
+  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
