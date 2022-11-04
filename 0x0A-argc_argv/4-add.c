c93b966a0df13c02cc8a2d62badce9977ca8283e
Sydney <yendev01@gmail.com>
2022-11-04 21:50:46 +0000

    I know the flaws in the code

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+  * main - program that adds positive numbers
+  * @argc: count
+  * @argv: string array input
+  * Return: 0 or 1
+  */
+
+int main(int argc, char* argv[])
+{
+	int sum = 0, i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i][0]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
