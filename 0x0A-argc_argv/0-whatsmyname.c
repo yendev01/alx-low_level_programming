e5178143eef07f6cab943b803287405ad3d0e136
Sydney <yendev01@gmail.com>
2022-11-04 20:32:47 +0000

    argc & argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * @argc: count
+  * @argv: argument vector(1-D array)
+  * Return: returns 0 
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
