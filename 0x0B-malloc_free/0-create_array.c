24b073c0e37e4a12f6ea2c60a5144b00c082da5c
Sydney <yendev01@gmail.com>
2022-11-08 09:37:18 +0000

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	ptr = malloc(size * sizeof(int));
+	ptr = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 	{
