c5b416042c32fd643981887f40005921a9503afc
Sydney <yendev01@gmail.com>
2022-11-08 11:02:50 +0000

    minor fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,24 +13,29 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	unsigned int i, length = strlen(s1) + strlen(s2);
 
-	if ((s1 == NULL) || (s2 == NULL))
-	{
-		return (NULL);
-	}
-
 	ptr = (char *)malloc((length + 1) * sizeof(char));
 
-	for (i = 0; *s1 != '\0'; i++)
+	if ((s1 == NULL) && (s2 == NULL))
 	{
-		*(ptr + i) = *s1;
-		s1++;
+		return (ptr);
 	}
-	*(ptr + i) = ' ';
 
-	for (; i <= length; ++i)
+	if (s1 != NULL)
 	{
-		*(ptr + i) = *s2;
-		s2++;
+		for (i = 0; *s1 != '\0'; i++)
+		{
+			*(ptr + i) = *s1;
+			s1++;
+		}
+		*(ptr + i) = ' ';
+	}
+	if (s2 != NULL)
+	{
+		for (; i <= length; ++i)
+		{
+			*(ptr + i) = *s2;
+			s2++;
+		}
 	}
 
 	*(ptr + i) = '\0';
