496cd8f677980ba82d6933ca4c24763d09fe8d06
Sydney <yendev01@gmail.com>
2022-11-11 08:10:37 +0000

    malloc for 3d solved

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+
+/**
+  * alloc_grid - function that returns a pointer to a 2d array of int
+  * @width: width of array
+  * @height: height of array
+  * Return: returns a pointer to a 2d array
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int **array = malloc(height * sizeof(**array)), i, n, m;
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	for (n = 0; n < height; n++)
+	{
+		for (m = 0; m < height; m++)
+		{
+			array[n][m] = 0;
+		}
+	}
+
+	return (array);
+}
