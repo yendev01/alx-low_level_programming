2118bc7becded58ebfce2b1d492fc38efa07f664
Sydney <yendev01@gmail.com>
2022-11-11 08:41:48 +0000

    casting error fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	array = malloc(height * sizeof(**array));
+	array = malloc(height * sizeof(int *));
 
 	if (array == NULL)
 	{
@@ -29,10 +29,12 @@ int **alloc_grid(int width, int height)
 		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
 		{
-			for (i--; i >= 0; i--)
-			{
-				free(array[i]);
-			}
+		/*
+		 *  for (i--; i >= 0; i--)
+		 *	{
+		 *		free(array[i]);
+		 *	}
+		 */
 			free(array);
 			return (NULL);
 		}
