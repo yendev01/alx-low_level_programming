8a17e2c9eb7a9d7dda50a865ddaa79028d67e9eb
Sydney <yendev01@gmail.com>
2022-11-12 08:14:27 +0000

    malloc checker

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+  * malloc_checked - function that allocates memory using malloc
+  * @b: input
+  * Return: returns nothing
+  */
+
+void *malloc_checked(unsigned int b)
+{
+	void *reserve;
+	
+	if (b == 0)
+	{
+		exit(98);
+	}
+	reserve = malloc(b);
+	
+	if (reserve == NULL)
+	{
+		exit(98);
+	}
+
+	return ((void *)reserve);
+}
