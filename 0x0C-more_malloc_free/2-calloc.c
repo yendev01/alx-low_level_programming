ef2e7a43e3f93da0cf3470f00e5c26675167efda
Sydney <yendev01@gmail.com>
2022-11-12 09:57:54 +0000

    fixing

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array;
-	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -23,11 +22,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-
-	for (i = 0; i < nmemb; i++)
-	{
-		array[i] = 0;
-	}
-
 	return ((void *)array);
 }
