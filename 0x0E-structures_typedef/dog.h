38287ead080cd813d5330c597d3a8ad819fba06f
Sydney <yendev01@gmail.com>
2022-11-16 14:50:20 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+  * struct dog - structure to facilitate description of dogs
+  * @name: name
+  * @age: age
+  * @owner: owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
