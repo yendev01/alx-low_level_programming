2ae5685716dbcf2e8e91c08e548d5c3df299d8e3
Sydney <yendev01@gmail.com>
2022-11-17 07:38:44 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * free_dog - function that frees dogs
+  * @d: input
+  * Return: returns nothing
+  */
+
+void free_dog (dog_t *d)
+{
+	if (d)
+	{	
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
