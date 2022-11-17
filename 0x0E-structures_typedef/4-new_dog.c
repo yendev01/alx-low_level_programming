4d5a5f2e49f315c0b1a4d29667eee0b8cc971b04
Sydney <yendev01@gmail.com>
2022-11-17 07:08:02 +0000

    new day

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+  * new_dog - function
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  * Return: returns a pointer to that instance
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggy;
+
+	if (!name || !owner)
+		return (NULL);
+
+	doggy = malloc(sizeof(dog_t));
+
+	if (!doggy)
+		return (NULL);
+
+	doggy->name = malloc(sizeof(char) * strlen(name) + 1);
+	doggy->owner = malloc(sizeof(char) * strlen(name) + 1);
+	if (!doggy->name || !doggy->owner)
+	{
+		free(doggy);
+		return (NULL);
+	}
+	strcpy(doggy->name, name);
+	strcpy(doggy->owner, owner);
+	doggy->age = age;
+
+	return (doggy);
+}
