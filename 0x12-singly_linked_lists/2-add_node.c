4fc993f25f3bbcbf53208434fb13a6e6209ac9b1
Sydney <yendev01@gmail.com>
2022-11-28 09:25:06 +0000

    handling edge cases

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,6 +13,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *h = malloc(sizeof(list_t));
 
+	if (!str)
+		return (NULL);
 	h->str = strdup(str);
 	h->len = (unsigned int)strlen(str);
 	h->next = *head;
