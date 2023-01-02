5dd3491d836bc90f41d71a9cf9a6b9587195f992
Sydney <yendev01@gmail.com>
2023-01-02 14:11:15 +0000

    minor fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 1;
+	size_t count = 0;
 
-	while (h->next != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
-	
-	printf("%d\n", h->n);
 
 	return (count);
 }
