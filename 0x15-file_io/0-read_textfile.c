7ff76c36b9d98f7466aea2faf71fdc0ba3b448c5
Sydney <yendev01@gmail.com>
2023-01-07 15:47:38 +0000

    debugging

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,6 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
+	buf[rd] = '\0';
 	wd = write(STDOUT_FILENO, buf, rd);
 
 	if (wd == -1)
@@ -39,6 +40,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buf);
 		return (0);
 	}
+	free(buf);
 	close(fd);
 	return (wd);
 }
