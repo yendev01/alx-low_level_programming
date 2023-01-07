ebe686431346c973fbba1bebec912d96b935a480
Sydney <yendev01@gmail.com>
2023-01-07 15:43:47 +0000

    debugging

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t wd, rd;
-	char buf[1024];
+	char *buf;
 
 	if (!*filename)
 		return (0);
@@ -21,16 +21,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
+	buf = malloc((size_t)sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
 	rd = read(fd, buf, letters);
 
 	if (rd == -1)
+	{
+		free(buf);
 		return (0);
-
+	}
 	wd = write(STDOUT_FILENO, buf, rd);
 
 	if (wd == -1)
+	{
+		free(buf);
 		return (0);
-
-	close (fd);
+	}
+	close(fd);
 	return (wd);
 }
