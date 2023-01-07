54c2f8f345d1765b350e9d83117ea067f96c6920
Sydney <yendev01@gmail.com>
2023-01-07 15:10:28 +0000

    FILE I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+  * read_textfile - function that reads a text file and prints to stdout
+  * @filename: filename from which it is read
+  * @letters: bytes
+  * Return: ...
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t wd, rd;
+	char *buf = malloc((size_t)sizeof(char) * letters);
+
+	if (!buf)
+		return (0);
+
+	if (!*filename)
+		return (0);
+
+	fd = open("Requiescat", O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	rd = read(fd, buf, letters);
+
+	if (rd == -1)
+		return (0);
+
+	wd = write(1, buf, rd);
+
+	if (wd == -1)
+		return (0);
+
+	return (wd);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef H_LIST
+#define H_LIST
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
