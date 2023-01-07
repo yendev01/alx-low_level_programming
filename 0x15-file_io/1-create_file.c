0ef46614ed2f99c337da1419c05d8d1355199691
Sydney <yendev01@gmail.com>
2023-01-07 18:07:29 +0000

    minor fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd < 0)
 		return (-1);
