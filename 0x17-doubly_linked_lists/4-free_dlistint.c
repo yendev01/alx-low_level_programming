a88d1aa4b0a7007cd5c902a0fa891461ab622800
Sydney <yendev01@gmail.com>
2023-01-15 08:51:51 +0000

    frees a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - frees a dlistint_t list
+  * @head: ...
+  * Return: void(nothing)
+  */
+
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
