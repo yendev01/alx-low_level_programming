0c791e368ff97080f3d80795a8041d67df0be04d
Sydney <yendev01@gmail.com>
2023-01-15 09:05:23 +0000

    debugging

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,6 +12,9 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *indexNode = head;
 
+	if (!head)
+		return (NULL);
+
 	while (i < index)
 	{
 		indexNode = indexNode->next;
